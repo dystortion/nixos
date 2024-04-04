fbf24106c83709c5c9671f7b9f68b44ff34b5733
dystortion <dystortion@posteo.net>
2024-04-04 14:29:42 +0530

    Added htop settings, github and codeberg keys
    	modified: nix/nixosConfigurations/illumi/programs/default.nix
    	modified: nix/nixosConfigurations/illumi/programs/git.nix

--- nix/nixosConfigurations/illumi/programs/default.nix
+++ nix/nixosConfigurations/illumi/programs/default.nix
@@ -21,17 +21,42 @@
     };
     dconf.enable = true;
     direnv.enable = true;
-    gnupg.agent = {
+    gnupg.agent.enable = true;
+    htop = {
       enable = true;
-      enableSSHSupport = true;
+      settings = {
+        hide_userland_threads = true;
+        shadow_other_users = true;
+        show_thread_names = true;
+        show_program_path = true;
+        highlight_base_name = true;
+        highlight_deleted_exe = true;
+        shadow_distribution_path_prefix = true;
+        detailed_cpu_time = true;
+        show_cpu_frequency = true;
+        show_cpu_temperature = true;
+        update_process_names = true;
+        delay = 10;
+        column_meters_0 = [ "LeftCPUs2" "Memory" "Zram" "DiskIO" "NetworkIO" ];
+        column_meter_modes_0 = [ 1 1 1 1 1 ];
+        column_meters_1 = [ "RightCPUs2" "Tasks" "LoadAverage" "Uptime" "Systemd" ];
+        column_meter_modes_1 = [ 1 2 2 2 2 ];
+      };
     };
-    htop.enable = true;
     iftop.enable = true;
     iotop.enable = true;
     kdeconnect.enable = true;
     mtr.enable = true;
     nix-ld.enable = true;
     partition-manager.enable = true;
+    ssh = {
+      startAgent = true;
+      knownHosts = {
+        "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+        "codeberg.org".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIVIC02vnjFyL+I4RHfvIGNtOgJMe769VTF1VR4EB3ZB";
+      };
+    };
+    steam.enable = true;
     trippy.enable = true;
     wavemon.enable = true;
     wireshark = {
--- nix/nixosConfigurations/illumi/programs/git.nix
+++ nix/nixosConfigurations/illumi/programs/git.nix
@@ -3,6 +3,8 @@
   programs = {
     git = {
       enable = true;
+      package = pkgs.gitFull;
+      lfs.enable = true;
       config = {
         alias = {
           br = "branch";
